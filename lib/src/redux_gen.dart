9ea6f45f744e4e645d9c3b520a3ba63efc75d6c0
Yura Shevelev <yurashevelev@appvesto.com>
2021-01-02 21:40:16 +0200

    Updated output

--- lib/src/redux_gen.dart
+++ lib/src/redux_gen.dart
@@ -11,29 +11,29 @@ class ReduxEpicGenerator extends GeneratorForAnnotation<Epic> {
     ConstantReader annotation,
     BuildStep buildStep,
   ) {
-    final StringBuffer _buffer = StringBuffer();
+    String _buffer = '';
 
-    print('element.displayName: ${element.displayName}');
-    print('element.hasFactory: ${element.hasFactory}');
-    print('element.id: ${element.id}');
-    print('element.name: ${element.name}');
-    print('element.location: ${element.location}');
-    print('element.library: ${element.library}');
-    print('element.librarySource: ${element.librarySource}');
+    print('element.displayName: ${element?.displayName}');
+    print('element.hasFactory: ${element?.hasFactory}');
+    print('element.id: ${element?.id}');
+    print('element.name: ${element?.name}');
+    print('element.location: ${element?.location}');
+    print('element.library: ${element?.library}');
+    print('element.librarySource: ${element?.librarySource}');
 
-    print('element.inputId: ${buildStep.inputId}');
-    print('element.inputLibrary: ${buildStep.inputLibrary}');
+    print('element.inputId: ${buildStep?.inputId}');
+    print('element.inputLibrary: ${buildStep?.inputLibrary}');
 
-    _buffer.write('element.displayName: ${element.displayName}\n');
-    _buffer.write('element.hasFactory: ${element.hasFactory}\n');
-    _buffer.write('element.id: ${element.id}\n');
-    _buffer.write('element.name: ${element.name}\n');
-    _buffer.write('element.location: ${element.location}\n');
-    _buffer.write('element.library: ${element.library}\n');
-    _buffer.write('element.librarySource: ${element.librarySource}\n');
+    _buffer += 'element.displayName: ${element?.displayName}\n';
+    _buffer += 'element.hasFactory: ${element?.hasFactory}\n';
+    _buffer += 'element.id: ${element?.id}\n';
+    _buffer += 'element.name: ${element?.name}\n';
+    _buffer += 'element.location: ${element?.location}\n';
+    _buffer += 'element.library: ${element?.library}\n';
+    _buffer += 'element.librarySource: ${element?.librarySource}\n';
 
-    _buffer.write('element.inputId: ${buildStep.inputId}\n');
-    _buffer.write('element.inputLibrary: ${buildStep.inputLibrary}\n');
+    _buffer += 'element.inputId: ${buildStep?.inputId}\n';
+    _buffer += 'element.inputLibrary: ${buildStep?.inputLibrary}\n';
 
     return _buffer.toString();
   }
