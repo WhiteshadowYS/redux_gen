652cd4a151690463b32e63f1aaf607835431e29a
Yura Shevelev <yurashevelev@appvesto.com>
2021-01-02 21:35:23 +0200

    Added prints

--- lib/src/redux_gen.dart
+++ lib/src/redux_gen.dart
@@ -11,6 +11,30 @@ class ReduxEpicGenerator extends GeneratorForAnnotation<Epic> {
     ConstantReader annotation,
     BuildStep buildStep,
   ) {
-    return "// Hey! Annotation found!";
+    final StringBuffer _buffer = StringBuffer();
+
+    print('element.displayName: ${element.displayName}');
+    print('element.hasFactory: ${element.hasFactory}');
+    print('element.id: ${element.id}');
+    print('element.name: ${element.name}');
+    print('element.location: ${element.location}');
+    print('element.library: ${element.library}');
+    print('element.librarySource: ${element.librarySource}');
+
+    print('element.inputId: ${buildStep.inputId}');
+    print('element.inputLibrary: ${buildStep.inputLibrary}');
+
+    _buffer.write('element.displayName: ${element.displayName}\n');
+    _buffer.write('element.hasFactory: ${element.hasFactory}\n');
+    _buffer.write('element.id: ${element.id}\n');
+    _buffer.write('element.name: ${element.name}\n');
+    _buffer.write('element.location: ${element.location}\n');
+    _buffer.write('element.library: ${element.library}\n');
+    _buffer.write('element.librarySource: ${element.librarySource}\n');
+
+    _buffer.write('element.inputId: ${buildStep.inputId}\n');
+    _buffer.write('element.inputLibrary: ${buildStep.inputLibrary}\n');
+
+    return _buffer.toString();
   }
 }
